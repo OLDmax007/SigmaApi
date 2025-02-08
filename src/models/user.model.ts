ece9e2936ba1daeb69e0c81d5f03d4efec45f4ee
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-02-08 13:56:55 +0200

    Added new files to user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import * as mongoose from "mongoose";
 
+import { GenderEnum } from "../enums/gender.enum";
+import { RoleEnum } from "../enums/role.enum";
 import { IUser } from "../interfaces/user.interface";
 
 const Schema = mongoose.Schema;
@@ -8,10 +10,13 @@ const UserSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    age: { type: Number, required: true },
+    gender: { type: GenderEnum, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     statusMarried: { type: Boolean, required: false, default: false },
+    role: { type: RoleEnum, required: false, default: RoleEnum.GUEST },
   },
   {
     timestamps: true,
