436f2cd11d64b0bfe30f000a257c412119b0eae8
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-02-08 14:46:40 +0200

    Created file user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,15 @@
+class UserService {
+  public async getItems() {}
+
+  public async getByFilters() {}
+
+  public async getById() {}
+
+  public async getByEmail() {}
+
+  public async update() {}
+
+  public async delete() {}
+}
+
+export const userService = new UserService();
