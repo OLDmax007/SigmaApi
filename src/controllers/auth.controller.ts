1df2eafd0be9df19e57b49df02f903f33fbfc457
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-02-08 18:40:53 +0200

    Created auth router, controller and service

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+
+import logger from "../helpers/logger.helper";
+
+class AuthController {
+  public async singUp(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (e) {
+      logger.error(e);
+      next(e);
+    }
+  }
+
+  public async singIn(req: Request, res: Response, next: NextFunction) {
+    try {
+    } catch (e) {
+      logger.error(e);
+      next(e);
+    }
+  }
+}
+
+export const authController = new AuthController();
---
+++ src/routes/auth.router.ts
@@ -0,0 +1,6 @@
+import {Router} from "express";
+
+export const authRouter = Router();
+
+authRouter.get("/sing-up");
+authRouter.get("/sing-in");
