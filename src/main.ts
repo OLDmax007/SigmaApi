1489daa820a78ac733dfc6e2394c7e48efdda719
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2025-02-09 01:07:24 +0200

    Set up handler error

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,20 @@ const app = express();
 const start = async () => {
   app.use("/api/users", userRouter);
 
+  app.use(
+    "*",
+    (error: ApiError, req: Request, res: Response, next: NextFunction) => {
+      const status = error.status || 500;
+      const message = error.message || "Something bad";
+      res.status(status).json({ status, message });
+    }
+  );
+
+  process.on("uncaughtException", (error) => {
+    console.error("Uncaught Exception:", error);
+    process.exit(1);
+  });
+
   app.listen(config.basic.port, async () => {
     await mongoose.connect(config.database.url);
     console.log(config.basic.frontUrl + config.basic.port);
